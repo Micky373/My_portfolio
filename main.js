aa2a9da871736b39006e0dc3905d681f4b491008
Michael Tamirie <michaeltamirie@gmail.com>
2022-01-25 23:37:23 +0300

    Fix eslint errors

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const hamburger = document.getElementById('hamburger')
-const menuItems = document.querySelector('.menu_items')
-const mobileMenu = document.querySelector('.mobile_menu')
-const closeButton = document.querySelector('.close_button')
-const gotoItems = document.querySelectorAll('.goto')
+const hamburger = document.getElementById('hamburger');
+const menuItems = document.querySelector('.menu_items');
+const mobileMenu = document.querySelector('.mobile_menu');
+const closeButton = document.querySelector('.close_button');
+const gotoItems = document.querySelectorAll('.goto');
 function toggling() {
-    menuItems.classList.toggle("appear");
-    mobileMenu.classList.toggle("open");
+  menuItems.classList.toggle('appear');
+  mobileMenu.classList.toggle('open');
 }
-hamburger.addEventListener('click', toggling)
-closeButton.addEventListener('click', toggling)
-for (let i = 0; i < gotoItems.length; i++) {
-    gotoItems[i].onclick = toggling
+hamburger.addEventListener('click', toggling);
+closeButton.addEventListener('click', toggling);
+for (let i = 0; i < gotoItems.length; i += 1) {
+  gotoItems[i].onclick = toggling;
 }
