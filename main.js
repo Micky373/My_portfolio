d95c44ffbb37319c475b4401298ce055672a8d52
Micky373 <michaeltamirie@gmail.com>
2023-01-18 15:22:53 +0300

    Finaliz the portfolio

--- main.js
+++ main.js
@@ -14,76 +14,57 @@ for (let i = 0; i < gotoItems.length; i += 1) {
 }
 
 /* Populating the portfolio */
+
 const projects = [
   {
-    title: 'Minimalist to-do list project',
-    languages: ['HTML and CSS', 'Javascript', 'Webpack'],
-    imageUrl: './images/To-do.png',
-    body: "In this minimalist to do list project I have used the best practice of java script, HTML and CSS. This project is fully responsive both for desktop and also for mobile version. Java script has been used to populate the HTML elements and CSS has been used to give the website a better look. As a measurement unit I have used rem and em to make it more adaptive to different vendors",
-    desc: "This is an easy responsive to-do list web application. You can write your tasks, edit, check completed or delete tasks. It is completely responsive and easy to use website application",
+    title: 'End to End sentiment analysis project',
+    languages: ['Python', 'TextBlob', 'NLP'],
+    imageUrl: './images/sentiment.gif',
+    body: "In this project I have done NLP and created a web app to display sentiment and subjectivity of the sentence.",
+    desc: "This is an easy responsive website that a user can pass a sentence and get the sentiment and subjectivity of it.",
     id: 1,
-    seeLiveUrl: 'https://micky373.github.io/To_DO_List/dist/',
-    seeSourceUrl: 'https://github.com/Micky373/To_DO_List',
+    seeLiveUrl: 'https://github.com/Micky373/end_to_end_sentiment_analysis',
+    seeSourceUrl: 'https://github.com/Micky373/end_to_end_sentiment_analysis',
   },
   {
-    title: 'Professional Art Printing Data',
-    languages: ['html', 'Bootstrap', 'Ruby'],
-    imageUrl: './images/portfolio_image.svg',
-    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    title: 'End to End Foot Ball players image classification',
+    languages: ['Python', 'Flask', 'SVM'],
+    imageUrl: './images/football.gif',
+    body: "In this repo I used a simple machine learning model to classify an image among my best five players. Lionel Messi, Cristiano Ronaldo, Ronaldinho, Patrick Vieira and Thierry Henry. I have created a simple UI to drag and drop images of players and used back end implementation for player name prediction. ",
+    desc: "This is a simple web app that you can drop a football player image and the machine learning model will predict the name of the player. I used 5 famous players in the project.",
     id: 2,
-    seeLiveUrl: 'https://micky373.github.io/My_portfolio/',
-    seeSourceUrl: 'https://github.com/Micky373/My_portfolio',
+    seeLiveUrl: 'https://github.com/Micky373/end_to_end_football_players_image_classification',
+    seeSourceUrl: 'https://github.com/Micky373/end_to_end_football_players_image_classification',
   },
   {
-    title: 'Professional Art Printing Data',
-    languages: ['html', 'Bootstrap', 'Ruby'],
-    imageUrl: './images/portfolio_image.svg',
-    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    title: 'End to End Home Price prediction',
+    languages: ['Python', 'Flask', 'Linear Regression'],
+    imageUrl: './images/home_price.png',
+    body: "In this project I have tried to do some EDA on the home price dataset and run different machine learning models to check which model gives the best solution with a good parameter. After getting the best model and saving it then I used Flask for deploying the model. ",
+    desc: "This is a web app that we will able to pass some features of the house and get the price prediction based on the trained model. This will help us easily find the best suit home for us.",
     id: 3,
-    seeLiveUrl: 'https://micky373.github.io/My_portfolio/',
-    seeSourceUrl: 'https://github.com/Micky373/My_portfolio',
+    seeLiveUrl: 'https://github.com/Micky373/end_to_end_home_price_prediction_ml_project',
+    seeSourceUrl: 'https://github.com/Micky373/end_to_end_home_price_prediction_ml_project',
   },
   {
-    title: 'Professional Art Printing Data',
-    languages: ['html', 'Bootstrap', 'Ruby'],
-    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    imageUrl: './images/portfolio_image.svg',
-    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    title: 'Potato Disease classification using deep learning',
+    languages: ['Python', 'Flask', 'TensorFlow'],
+    desc: "This is a web app in which a user can drag and drop a potato image and the model will predict whether the potato is healthy or not",
+    imageUrl: './images/potato.gif',
+    body: "In this project I trained a CNN model and predicted three types of potato leaf. Either the potato may be healthy or has an early blight disease or late blight disase. The model has good accuracy on these 3 classes. But it is accepting only images of size (256,256) if we pass images other than that shape it won't work. ",
     id: 4,
     seeLiveUrl: 'https://micky373.github.io/My_portfolio/',
     seeSourceUrl: 'https://github.com/Micky373/My_portfolio',
   },
   {
-    title: 'Professional Art Printing Data',
-    languages: ['html', 'Bootstrap', 'Ruby'],
-    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    imageUrl: './images/portfolio_image.svg',
-    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    title: 'Multiple AI projects',
+    languages: ['Python', 'Jupyter Notebook', 'Machine Learning'],
+    desc: "This is a simple web app that we can pass multiple feature measures and the model will be able to predict whether the person has a disease or not.",
+    imageUrl: './images/ai_project.png',
+    body: "In this repo I have done multiple machine learning and deep learning projects. I have created a simple web app for two simpler disease classification models. Those are the diabetes and heart disease classifications. It is an ongoing project and more web apps for more models will be deployed in the near future.",
     id: 5,
-    seeLiveUrl: 'https://micky373.github.io/My_portfolio/',
-    seeSourceUrl: 'https://github.com/Micky373/My_portfolio',
-  },
-  {
-    title: 'Professional Art Printing Data',
-    languages: ['html', 'Bootstrap', 'Ruby'],
-    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    imageUrl: './images/portfolio_image.svg',
-    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-    id: 6,
-    seeLiveUrl: 'https://micky373.github.io/My_portfolio/',
-    seeSourceUrl: 'https://github.com/Micky373/My_portfolio',
-  },
-  {
-    title: 'Professional Art Printing Data',
-    languages: ['html', 'Bootstrap', 'Ruby'],
-    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    imageUrl: './images/portfolio_image.svg',
-    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-    id: 7,
-    seeLiveUrl: 'https://micky373.github.io/My_portfolio/',
-    seeSourceUrl: 'https://github.com/Micky373/My_portfolio',
+    seeLiveUrl: 'https://micky373-ai-projects-multiple-disease-prediction-0bpixm.streamlit.app/',
+    seeSourceUrl: 'https://github.com/Micky373/AI_projects',
   },
 ];
 
@@ -91,7 +72,7 @@ const portfolio = document.getElementById('Portfolio');
 let template = '';
 for (let i = 1; i < projects.length; i += 1) {
   template += `
-  <div class="grid-item">
+  <div class="grid-item" style="background: linear-gradient(180.45deg, rgba(38, 38, 38, 0) 0.75%, rgba(38, 38, 38, 0.9) 61.94%), url(${projects[i].imageUrl});background-size: 100% 100%;background-repeat: no-repeat;">
       <div class="grid-item-headline">
           <h3>${projects[i].title}</h3>
           <p>${projects[i].desc}</p>
@@ -131,6 +112,9 @@ for (let i = 0; i < projects.length; i += 1) {
                 <div class="descriptive_content">
                     <div>
                         <p>${projects[i].body}</p>
+                    </div>
+                    <div>
+                        <p>${projects[i].desc}</p>
                     </div>
                     <div class="pop_up_links">
                         <a href='${projects[i].seeLiveUrl}'><button type="button">See Live&nbsp;&nbsp;<i class="fa fa-signal" aria-hidden="true"></i></button></a>
